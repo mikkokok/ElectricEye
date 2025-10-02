ac7003b75b4191068e54dd46a42e02d93c92161a
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2025-10-02 20:02:39 +0300

    Changed to 15 minutes pricing

--- Services/ChargerService.cs
+++ Services/ChargerService.cs
@@ -14,11 +14,12 @@ namespace ElectricEye.Services
         private readonly ILogger<ChargerService> _logger = logger;
         private readonly IRequestProvider _requestProvider = requestProvider;
         private List<PollerStatus> _pollerUpdates = [];
-        private int _lastHour;
+        private int _lastTime;
         private int _lastReading;
         private bool _initialPoll = true;
         public Task? CleanTask { get; private set; }
         public Task? ChargerTask;
+        public int _delaySeconds = 5;
 
         public List<PollerStatus> GetStatus()
         {
@@ -28,19 +29,21 @@ namespace ElectricEye.Services
         public async Task RunPoller(CancellationToken stoppingToken)
         {
             _logger.LogInformation($"{_serviceName}:: starting charger polling");
+            _lastTime = DateTime.Now.Minute;
             CleanTask = CleanUpdatesList(stoppingToken);
 
             while (!stoppingToken.IsCancellationRequested)
             {
                 try
                 {
-                    if (CalculateExactHour())
+                    if (CalculateExactMinute())
                     {
                         for (int i = 1; i < 3; i++)
                         {
                             try
                             {
                                 await ChargerCollector();
+                                await Task.Delay(TimeSpan.FromSeconds(60), stoppingToken);
                                 _logger.LogInformation($"{_serviceName}:: charger collecting success. ending loop. Cleanin task status {CleanTask.Status}");
                                 break;
                             }
@@ -60,14 +63,23 @@ namespace ElectricEye.Services
                                     _logger.LogInformation($"{_serviceName}:: {i}/3 retries, passed limit");
                                     throw new Exception("Retries done, exiting");
                                 }
-                                await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken);
+                                await Task.Delay(TimeSpan.FromSeconds(60), stoppingToken);
                             }
                         }
                     }
-                    await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken);
+                    if (DateTime.Now.Minute < 13 && DateTime.Now.Minute > 1)
+                    {
+                      _delaySeconds = 45;
+                    }
+                    else
+                    {
+                        _delaySeconds = 5;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(_delaySeconds), stoppingToken);
                 }
                 catch (Exception ex)
                 {
+                    _logger.LogError($"{_serviceName}:: polling failed, token {stoppingToken.IsCancellationRequested}, errormessage {ex.Message}");
                     _pollerUpdates.Add(new PollerStatus
                     {
                         Time = DateTime.Now,
@@ -77,6 +89,7 @@ namespace ElectricEye.Services
                     });
                 }
             }
+            await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
             _logger.LogInformation($"{_serviceName}:: ending carger polling, token {stoppingToken.IsCancellationRequested}");
         }
         private async Task ChargerCollector()
@@ -95,7 +108,7 @@ namespace ElectricEye.Services
             if (_lastReading < reading.eto && !_initialPoll)
             {
                 DateTime now = DateTime.Now;
-                DateTime rounded = new(now.Year, now.Month, now.Day, now.Hour, 0, 0);
+                DateTime rounded = new(now.Year, now.Month, now.Day, now.Hour, now.Minute, 0);
                 await SendChargingData(new CarCharge
                 {
                     date = rounded.AddHours(-1).ToString("yyyy-MM-dd HH:mm:ss").Replace(".", ":"),
@@ -117,11 +130,11 @@ namespace ElectricEye.Services
             _logger.LogInformation($"{_serviceName}:: ended run of ChargerCollector {DateTime.Now} lastReading: {_lastReading} initialPoll: {_initialPoll}");
         }
 
-        private bool CalculateExactHour()
+        private bool CalculateExactMinute()
         {
-            if (_lastHour < DateTime.Now.Hour || (_lastHour == 23 && DateTime.Now.Hour == 0))
+            if (_lastTime != DateTime.Now.Minute && ( DateTime.Now.Minute == 0 || DateTime.Now.Minute == 15 || DateTime.Now.Minute == 30 || DateTime.Now.Minute == 45))
             {
-                _lastHour = DateTime.Now.Hour;
+                _lastTime = DateTime.Now.Minute;
                 return true;
             }
             return false;
